d17c47a1f7ea6b3ea8a9b964f3ef59825395b8f2
Manuel-Lugrin <manuel_lugrin@live.com.ar>
2023-09-10 15:47:45 -0300

    Comenzamos con el codigo: Clases, Listado, Formulario principal

--- TPWinForm_equipo-11/TPWinforms_Catalogo.cs
+++ TPWinForm_equipo-11/TPWinforms_Catalogo.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Domain;
+using Business;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TPWinForm_equipo_11
 {
     public partial class TPWinforms_Catalogo : Form
     {
+        private List<Articulo> listaArticulo;
         public TPWinforms_Catalogo()
         {
             InitializeComponent();
+        }
+
+        private void TPWinforms_Catalogo_Load(object sender, EventArgs e)
+        {
+            ArticuloBusiness business = new ArticuloBusiness();
+            listaArticulo = business.listar();
+            dgvArticulo.DataSource = listaArticulo;
+            dgvArticulo.Columns["Id"].Visible = false;
+            dgvArticulo.Columns["Imagen"].Visible = false;
+        }
+
+        private void dvgArticulo_SelectionChanged(object sender, EventArgs e)
+        {
+            Articulo articuloSelecionado = (Articulo)dgvArticulo.CurrentRow.DataBoundItem;
+            cargarImagen(articuloSelecionado.Imagen.ImagenUrl);
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbxArticulo.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+                pbxArticulo.Load("https://efectocolibri.com/wp-content/uploads/2021/01/placeholder.png");
+            }
         }
     }
 }
